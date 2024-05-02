4f7a9d798a55f9ac022b466f01cfa2bbf9632469
Wafiyyah <diannahwafiyyah@gmail.com>
2024-05-02 22:37:17 +0800

    Community Post updated

--- website/views.py
+++ website/views.py
@@ -1,11 +1,30 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+from .models import Post
+from . import db
 
 views = Blueprint("views", __name__)
 
 @views.route("/")
 @views.route("/home")
 def home():
+    if request.method =="POST":
+        text = request.form.get('text')
+
+        if not text:
+            flash('Post cannot be empty', category='error')
+        else:
+            post = Post(text=text, author=current_user.id)
+            db.session.add(post)
+            db.session.commit()
+            flash('Post created!', category='success')
+
     return render_template("home.html")
+
+@views.route("/create-post", methods=['GET', 'POST'])
+@login_required
+def create_post():
+    return render_template('create_post.html', user=current_user)
 
 @views.route('/color')
 def color():
