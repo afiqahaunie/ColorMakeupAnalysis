10320005c0eacfe1fe489f8239463ee48ac503ca
Farisha Hani <farishahani1001@gmail.com>
2024-05-08 13:01:09 +0800

    logout function

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db 
 from .models import User
-from flask_login import login_user, login_required, current_user
+from flask_login import login_user, login_required, current_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint("auth", __name__)
@@ -23,7 +23,7 @@ def login():
        else: 
           flash('Email does not exist.', category='error')
 
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 @auth.route("/signup", methods=['GET','POST'])
 def signup():
@@ -57,9 +57,10 @@ def signup():
 
 
 
-    return render_template("signup.html")
+    return render_template("signup.html", user=current_user)
 
 @auth.route("/logout")
 @login_required
 def logout():
+    logout_user()
     return redirect(url_for("views.home"))  
