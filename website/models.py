d5131ae524ebe606ecae38bbeaca805aa4846f20
Wafiyyah <diannahwafiyyah@gmail.com>
2024-05-15 14:15:43 +0800

    Delete post and comment feature

--- website/models.py
+++ website/models.py
@@ -8,13 +8,20 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     posts =db.relationship('Post', backref='user', passive_deletes=True)
-    result = db.relationship('Result', backref='user_result', uselist=False)
+    comments =db.relationship('Comment', backref='post', passive_deletes=True)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text =db.Column(db.Text, nullable=False)
+    text = db.Column(db.Text, nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False) 
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+    comments = db.relationship('Comment', backref='post_comments', lazy=True)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200), nullable=False)
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
 
 class Upload(db.Model):
     id = db.Column(db.Integer, primary_key=True)
